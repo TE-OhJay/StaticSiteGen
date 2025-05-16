74c2a9d579620a753231dc1c6aafffa51bea8406
OhJay <ollejardbrink@gmail.com>
2025-05-16 09:50:09 +0200

    setup goes on, textnode-texttype

--- src/main.py
+++ src/main.py
@@ -1,1 +1,7 @@
-print("hello world")
+from textnode import TextNode, TextType
+
+def main():
+    dummy_node = TextNode("Testing on a dummy", TextType.TEXT, None)
+    print(dummy_node)
+    
+main()
